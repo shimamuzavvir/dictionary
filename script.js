6f95909cab81bab82e5f2216a05848f940979dea
shimamuzavvir <shima1997ibrahim@gmail.com>
2024-02-12 15:14:10 +0400

    s

--- script.js
+++ script.js
@@ -8,33 +8,36 @@ function fetchDefinition() {
     fetch(url)
     .then(response => response.json())
     .then(data => {
+        console.log(data);
         var meanings = data[0].meanings;
+       // console.log(meanings);
+       
+   // Extracting definition
+   var definition = meanings[0].definitions[0].definition;
+   document.getElementById("definition").innerHTML = `<b>Definition: <i>  ${definition} </i></b>`;
+   
+   // Extracting synonyms
+   if (meanings[0].definitions[0].synonyms) {
+       var synonyms = meanings[0].definitions[0].synonyms.join(", ");
+       document.getElementById("synonyms").innerText = "Synonyms: " + synonyms;
+   } else {
+       document.getElementById("synonyms").innerText = "Synonyms: Not available";
+   }
+   
+   // Extracting audio pronunciation
+   if (data[0].phonetics && data[0].phonetics[0].audio) {
+       var audioUrl = data[0].phonetics[0].audio;
+       var audioElement = document.createElement("audio");
+       audioElement.src = audioUrl;
+       audioElement.controls = true;
+       document.getElementById("audio").innerHTML = "";
+       document.getElementById("audio").appendChild(audioElement);
+   } else {
+       document.getElementById("audio").innerText = "Audio: Not available";
+   }      
+   //clearing input value
+   document.getElementById("wordInput").innerHTML = ""
 
-        
-        // Extracting definition
-        var definition = meanings[0].definitions[0].definition;
-        document.getElementById("definition").innerHTML = `<b>Definition: <i>  ${definition} </i></b>`;
-        
-        // Extracting synonyms
-        if (meanings[0].definitions[0].synonyms) {
-            var synonyms = meanings[0].definitions[0].synonyms.join(", ");
-            document.getElementById("synonyms").innerText = "Synonyms: " + synonyms;
-        } else {
-            document.getElementById("synonyms").innerText = "Synonyms: Not available";
-        }
-        
-        // Extracting audio pronunciation
-        if (data[0].phonetics && data[0].phonetics[0].audio) {
-            var audioUrl = data[0].phonetics[0].audio;
-            var audioElement = document.createElement("audio");
-            audioElement.src = audioUrl;
-            audioElement.controls = true;
-            document.getElementById("audio").innerHTML = "";
-            document.getElementById("audio").appendChild(audioElement);
-        } else {
-            document.getElementById("audio").innerText = "Audio: Not available";
-        }
-      
     })
     .catch(error => {
         console.log("Error fetching definition:", error);
